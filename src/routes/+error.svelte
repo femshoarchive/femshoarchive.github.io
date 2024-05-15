c4edc8599621621e074641e0a8bc1b69440a7a6e
Dark Steveneq <dark.steveneq@protonmail.com>
2024-05-15 11:32:34 +0200

    Implement 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<section>
+    <div />
+    <img src="/art/therealxneryvevo/shotwinkcry.webp" alt="">
+    <h1>Something ain't right!</h1>
+    Lemme know what page you were trying to go to on Discord (@darksteveneq) so I can fix the thing 
+</section>
+
+<style>
+section {
+    font-size: 16pt;
+    font-weight: 300;
+    text-align: center;
+    overflow: hidden;
+    padding-bottom: 50px;
+}
+
+div {
+    z-index: -10;
+    position: fixed;
+    background-color: #ffffff;
+    width: 100vw;
+    height: 100vh;
+}
+
+section h1 {
+    font-size: 32pt;
+}
+
+@media screen and (max-width: 1000px) {
+    section img {
+        width: 100vw;
+    }
+}
+</style>
