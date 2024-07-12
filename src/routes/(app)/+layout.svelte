10084b63e2f8353f22f0f1c9a966d85dc54ebd35
Dark <dark.steveneq@protonmail.com>
2024-07-12 15:15:06 +0200

    Update packages, move PostCSS stuff to a separate file and style h2 elements

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
     import Header from './Header.svelte';
+    import "../../app.css";
 </script>
 
 <Header/>
 <section class="w-5/6 m-auto mt-3 p-2 text-black dark:text-slate-200 bg-slate-50 dark:bg-slate-800 rounded-lg drop-shadow-lg shadow-slate-400">
     <slot/>
 </section>
-
-<style global lang="postcss">
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-</style>
